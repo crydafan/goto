9b141330e3d23629bb367df100389df2ad226837
Leonardo <leonardo.estacio.h@gmail.com>
2025-12-02 18:22:31 -0500

    feat: static page

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { Elysia } from "elysia";
+import { staticPlugin } from "@elysiajs/static";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+    staticPlugin({
+      assets: "public",
+      prefix: "/",
+    })
+  )
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
